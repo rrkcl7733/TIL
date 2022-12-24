43a96d364b52f66e608016b7581fb22c4b9163a8
rrkcl7733 <rrkcl7733@naver.com>
2022-12-24 14:06:51 +0900

    print

---
+++ baekjoon/브론즈/26772_Poziome serca.py
@@ -0,0 +1,27 @@
+n = int(input())
+for _ in range(n):
+    print(' @@@   @@@ ', end=' ')
+print()
+for _ in range(n):
+    print('@   @ @   @', end=' ')
+print()
+for _ in range(n):
+    print('@    @    @', end=' ')
+print()
+for _ in range(n):
+    print('@         @', end=' ')
+print()
+for _ in range(n):
+    print(' @       @ ', end=' ')
+print()
+for _ in range(n):
+    print('  @     @  ', end=' ')
+print()
+for _ in range(n):
+    print('   @   @   ', end=' ')
+print()
+for _ in range(n):
+    print('    @ @    ', end=' ')
+print()
+for _ in range(n):
+    print('     @     ', end=' ')
