e210a8a65aeaa6f781f13734abeaa03b5676b3d5
rrkcl7733 <rrkcl7733@naver.com>
2023-01-06 23:54:53 +0900

    Ada 언어 print

---
+++ baekjoon/브론즈/2372_Livestock Count.adb
@@ -0,0 +1,13 @@
+with Ada.Text_IO;
+
+procedure Hello_World is
+   use Ada.Text_IO;
+begin
+   Put_line ("Animal      Count");
+   Put_line ("-----------------");
+   Put_line ("Chickens      100");
+   Put_line ("Clydesdales     5");
+   Put_line ("Cows           40");
+   Put_line ("Goats          22");
+   Put_line ("Steers          2");
+end Hello_World;
