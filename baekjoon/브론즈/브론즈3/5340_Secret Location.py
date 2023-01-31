4717768b58fff329aa40ac13956b57d73e80f915
rrkcl7733 <rrkcl7733@naver.com>
2023-01-31 18:14:13 +0900

    rstrip

---
+++ baekjoon/브론즈/브론즈3/5340_Secret Location.py
@@ -0,0 +1,9 @@
+# 문장의 맨 마지막 공백은 제거하여야 한다
+a = len(input().rstrip(' '))
+b = len(input().rstrip(' '))
+c = len(input().rstrip(' '))
+print(f'Latitude {a}:{b}:{c}')
+a = len(input().rstrip(' '))
+b = len(input().rstrip(' '))
+c = len(input().rstrip(' '))
+print(f'Longitude {a}:{b}:{c}')
