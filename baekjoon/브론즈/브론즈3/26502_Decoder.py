fc095d1509b35a07222ec481e9d80cc3cae66aeb
rrkcl7733 <rrkcl7733@naver.com>
2023-06-16 20:36:52 +0900

    str.maketrans / str.translate

--- baekjoon/브론즈/브론즈3/26502_Decoder.py
+++ baekjoon/브론즈/브론즈3/26502_Decoder.py
@@ -1,12 +1,3 @@
-a = ['a', 'e', 'i', 'o', 'u', 'y']
-b = ['A', 'E', 'I', 'O', 'U', 'Y']
+a = str.maketrans('yaeiouYAEIOU', 'aeiouyAEIOUY')
 for _ in range(int(input())):
-    x = ''
-    for i in input():
-        if i in a:
-            x += a[(a.index(i) + 1) % 6]
-        elif i in b:
-            x += b[(b.index(i) + 1) % 6]
-        else:
-            x += i
-    print(x)
+    print(input().translate(a))
