a153e08bdf9ce10a86bc23d2e3eace2ea90d0dc5
rrkcl7733 <rrkcl7733@naver.com>
2023-06-27 09:49:47 +0900

    sys.stdin.readline

---
+++ baekjoon/브론즈/브론즈3/24724_현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+
+for i in range(int(input())):
+    n = int(input())
+    input()
+    [input() for _ in range(n)]
+    print(f'Material Management {i + 1}')
+    print('Classification ---- End!')
